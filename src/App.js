5b27ce8b4a84409a47cc64ea65221c3e29c4cb80
lisongyu <lisongyu@allinmd.cn>
2020-08-18 19:38:49 +0800

    提交文件提交

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 // import HocPage from './pages/HocPage'
-import MyFormPage from './pages/MyFormPage'
+// import MyFormPage from './pages/MyFormPage'
+import DialogPage from './pages/DialogPage'
 
 
-function App() {
-  return ( <
-    div className = "App" > {
-      /* 表单组件使用create */ } <
-    MyFormPage / >
+function App () {
+  return (<
+    div className="App" > {
+      /* 表单组件使用create */}
+    {/* < MyFormPage / >*/}
+    <DialogPage />
     <
     /div>
   )
